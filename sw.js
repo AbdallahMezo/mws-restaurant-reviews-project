8fe38813554ce1c014eaa564604321ebff08dcaa
Abdallah <aibrahim@iotblue.net>
2018-06-14 23:05:59 +0200

    - cache fix

--- sw.js
+++ sw.js
@@ -1,29 +1,4 @@
-const cacheName = 'resturants-cache-v1';
-const filesToCache = [
-  '/',
-  'js/dbhelper.js',
-  'js/main.js',
-  'js/restaurant_info.js',
-  'css/styles.css',
-  'img/1.jpg',
-  'img/2.jpg',
-  'img/3.jpg',
-  'img/4.jpg',
-  'img/5.jpg',
-  'img/6.jpg',
-  'img/7.jpg',
-  'img/8.jpg',
-  'img/9.jpg',
-  'img/10.jpg',
-  'img/icon.png',
-  'js/echo.min.js',
-  'index.html',
-  'restaurant.html',
-];
 
-const addToCache = (cache, files) => {
-  cache.addAll(files)
-}
 const sendOfflineRequests = (event) => {
   console.log('== event ==', event);
   self.sendReview();
@@ -32,9 +7,29 @@ const sendOfflineRequests = (event) => {
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(cacheName)
+    caches.open('resturants-cache-v1')
     .then(cache => {
-      return addToCache(cache, filesToCache);
+      cache.addAll([
+        '/',
+        'js/dbhelper.js',
+        'js/main.js',
+        'js/restaurant_info.js',
+        'css/styles.css',
+        'img/1.jpg',
+        'img/2.jpg',
+        'img/3.jpg',
+        'img/4.jpg',
+        'img/5.jpg',
+        'img/6.jpg',
+        'img/7.jpg',
+        'img/8.jpg',
+        'img/9.jpg',
+        'img/10.jpg',
+        'img/icon.png',
+        'js/echo.min.js',
+        'index.html',
+        'restaurant.html',
+      ]);
     })
   )
 });
