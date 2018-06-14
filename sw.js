062ba623ccce4e0fb9edcc58204398ea9a2935c1
Abdallah <aibrahim@iotblue.net>
2018-06-14 23:09:19 +0200

    - add logs

--- sw.js
+++ sw.js
@@ -7,8 +7,11 @@ const sendOfflineRequests = (event) => {
 
 self.addEventListener('install', event => {
   event.waitUntil(
+    console.log('== event in sw ==', event);
     caches.open('resturants-cache-v1')
     .then(cache => {
+    console.log('== caches in sw ==', caches);
+    console.log('== cache in sw ==', cache);
       cache.addAll([
         'js/dbhelper.js',
         'js/main.js',
